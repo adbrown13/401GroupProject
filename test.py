09d7a2ec8c5a961f6edca14b3faf6e7c2d5c8157
shawn <swanders@ualberta.ca>
2015-03-04 10:26:15 -0800

    fixed category chooser

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 import Tkinter
 import tkMessageBox
 import sound
-import idk
+
 import random
 
 
@@ -23,17 +23,17 @@ def mainloop():
 	type_of_file_to_play = random.choice(["1","2","3"])
 
 	if(type_of_file_to_play == "1"):
-		sound.run_audio
+		sound.run_audio()
 	#audiobutton = Tkinter.Button(label, text="Audio", font=("Helvetica",15), command = sound.run_audio)
 	#audiobutton.pack(fill=Tkinter.BOTH, expand=1)
 
 	if(type_of_file_to_play == "2"):
-		sound.run_muted
+		sound.run_muted()
 	#videobutton = Tkinter.Button(label, text="Video", font=("Helvetica",15), command = sound.run_muted)
 	#videobutton.pack(fill=Tkinter.BOTH, expand=1)
 
-	if(type_of_file_to_play == "2"):
-		sound.run_all
+	if(type_of_file_to_play == "3"):
+		sound.run_all()
 	#avbutton = Tkinter.Button(label, text="Audio-Video", font=("Helvetica",15), command = sound.run_all)
 	#avbutton.pack(fill=Tkinter.BOTH, expand=1)
 
