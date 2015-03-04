dc179de6bffa0a6978c734692aba8cc418ce288b
Afroholic78 <mickael.zerihoun@gmail.com>
2015-03-04 10:49:21 -0700

    added random category chooser

--- test.py
+++ test.py
@@ -4,12 +4,11 @@ import Tkinter
 import tkMessageBox
 import sound
 import idk
+import random
 
 
 def mainloop():
 	top = Tkinter.Tk()
-
-
 	top.title("Experiment")
 
 	var ="Choose the type of file you want to view"
@@ -21,27 +20,22 @@ def mainloop():
 	#top.overrideredirect(1)
 	top.geometry("%dx%d+0+0" % (w/2, h/2))
 
+	type_of_file_to_play = random.choice(["1","2","3"])
 
+	if(type_of_file_to_play == "1"):
+		sound.run_audio
+	#audiobutton = Tkinter.Button(label, text="Audio", font=("Helvetica",15), command = sound.run_audio)
+	#audiobutton.pack(fill=Tkinter.BOTH, expand=1)
 
+	if(type_of_file_to_play == "2"):
+		sound.run_muted
+	#videobutton = Tkinter.Button(label, text="Video", font=("Helvetica",15), command = sound.run_muted)
+	#videobutton.pack(fill=Tkinter.BOTH, expand=1)
 
-	def popup():
-		top.destroy()
-
-	audiobutton = Tkinter.Button(label, text="Audio", font=("Helvetica",15), command = sound.run_audio)
-	audiobutton.pack(fill=Tkinter.BOTH, expand=1)
-
-	videobutton = Tkinter.Button(label, text="Video", font=("Helvetica",15), command = sound.run_muted)
-	videobutton.pack(fill=Tkinter.BOTH, expand=1)
-
-	avbutton = Tkinter.Button(label, text="Audio-Video", font=("Helvetica",15), command = sound.run_all)
-	avbutton.pack(fill=Tkinter.BOTH, expand=1)
-
-
-
-	#B = Tkinter.Button(top, text = "hello", command = popup)
-	#B.pack()
-
-	# Code to add widgets will go here...
+	if(type_of_file_to_play == "2"):
+		sound.run_all
+	#avbutton = Tkinter.Button(label, text="Audio-Video", font=("Helvetica",15), command = sound.run_all)
+	#avbutton.pack(fill=Tkinter.BOTH, expand=1)
 
 
 	top.mainloop()
