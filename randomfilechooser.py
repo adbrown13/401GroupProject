36a58f45c749ffd1ea182a452d3191caee702c0b
kerr2 <kerr2@ualberta.ca>
2015-03-11 10:57:33 -0600

    Modifying randomfilechooser.  Returns an array containing randomized file names.

--- randomfilechooser.py
+++ randomfilechooser.py
@@ -1,3 +1,29 @@
 import os, random
 #Choose a random file from the /derp folder
-print random.choice(os.listdir(os.getcwd() + "/derp"))
+
+numOfTestFiles = 40
+numOfRepeatedFiles = int(numOfTestFiles * 0.1)
+numOfUniqueFiles = numOfTestFiles - numOfRepeatedFiles
+
+def chooseRandomFile():
+    chosenFiles = []
+    availableFiles = os.listdir( os.getcwd() + "/video" )
+
+    for i in range(1,numOfUniqueFiles):
+        randomfile = random.choice( list(set(availableFiles)-set(chosenFiles)) )
+        chosenFiles.append(randomfile)
+
+    repeatedFiles = []
+    repeatedFiles.append(chosenFiles[0])
+
+    for i in range(1,numOfUniqueFiles+1):
+        randomFile = random.choice( list(set(chosenFiles)-set(repeatedFiles)) )
+        repeatedFiles.append(randomFile)
+
+    for i in range(1,numOfUniqueFiles+1):
+        chosenFiles.insert(random.randint(1, numOfUniqueFiles-2+i), repeatedFiles[i])
+
+    chosenFiles.append(repeatedFiles[0])
+
+    return chosenFiles
+
